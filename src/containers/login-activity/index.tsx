1b91b45f04f55de07035ca2267a1e6b6018922dd
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-09 08:56:36 +0900

    Create login-activity

---
+++ src/containers/login-activity/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Container from '../container';
+
+export default class LoginActivity extends Container {
+  public render() {
+    return <div>Login Activity!</div>;
+  }
+}

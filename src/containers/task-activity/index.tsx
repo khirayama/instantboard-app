f4027f760ddccd446eee42b3610e61d3ee34be6d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-09 08:56:47 +0900

    Create task-activity

---
+++ src/containers/task-activity/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Container from '../container';
+
+export default class TaskActivity extends Container {
+  public render() {
+    return <div>Task Activity!</div>;
+  }
+}

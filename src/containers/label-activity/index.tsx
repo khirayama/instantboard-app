3f7066ad06322a043f92e99bf21eed80ba316773
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-09 08:56:27 +0900

    Create label-activity

---
+++ src/containers/label-activity/index.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import Container from '../container';
+
+export default class LabelActivity extends Container {
+  public render() {
+    return <div>Label Activity!</div>;
+  }
+}

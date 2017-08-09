46bcb1dd7180d3cdbb961ac58aa1d5fd275aa007
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-10 06:48:58 +0900

    Update reducers

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,4 +1,10 @@
+import * as deepAssign from 'deep-assign';
+import actionTypes from '../constants/action-types';
+
 export default function(state: IState, action: IAction): IState {
-  console.log(state, action);
-  return state;
+  let newState: IState = deepAssign({}, state);
+  const payload = action.payload;
+  const meta = action.meta;
+
+  return newState;
 };

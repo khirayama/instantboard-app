d5306aeca2cac513e3c53f2fc3aaa1e96651edab
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-13 10:11:32 +0900

    Update store interface

--- src/store/interface.d.ts
+++ src/store/interface.d.ts
@@ -20,10 +20,20 @@ interface IRequest {
 
 interface ILabel {
   id: string;
+  name: string;
+  visibled: boolean;
 }
 
 interface ITask {
   id: string;
+  labelId: string;
+  text: string;
+  completed: boolean;
+  schedule?: {
+    shortMonthName: string;
+    shortDayName: string;
+    date: number;
+  },
 }
 
 // UI

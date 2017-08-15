8f2fefdc5ee52409a5e236fefb9fadd90e83686d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-15 22:15:50 +0900

    Remove tab-navigation component

--- src/components/tab-navigation/tab-navigation-tab-list.tsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-export default class TabNavigationTabList extends React.Component<any, any> {
-  public render() {
-    return <div className="tab-navigation-tab-list">{this.props.children}</div>;
-  }
-}

9a4a7c75f11c003b12570fc9f59463051d1048b3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-23 23:08:19 +0900

    Remove handler in container components

--- src/containers/container.tsx
+++ src/containers/container.tsx
@@ -1,6 +1,8 @@
 import * as React from 'react';
 
 export default class Container<P extends IContainerProps, S extends IState> extends React.Component<any, any> {
+  protected handleStateUpdate: any;
+
   protected dispatch: (action: IAction) => void;
 
   protected actions: any;
@@ -8,6 +10,9 @@ export default class Container<P extends IContainerProps, S extends IState> exte
   constructor(props: P) {
     super(props);
 
+    this.handleStateUpdate = () => {
+      this.setState(props.store.getState());
+    };
     this.state = props.store.getState();
     this.dispatch = props.store.dispatch.bind(props.store);
   }
@@ -15,8 +20,12 @@ export default class Container<P extends IContainerProps, S extends IState> exte
   public componentWillMount() {
     const store = this.props.store;
 
-    store.addChangeListener(() => {
-      this.setState(store.getState());
-    });
+    store.addChangeListener(this.handleStateUpdate);
+  }
+
+  public componentWillUnmount() {
+    const store = this.props.store;
+
+    store.removeChangeListener(this.handleStateUpdate);
   }
 }

923c61de17e2352cdd21fb749dc93cb089a8999b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-27 08:54:16 +0900

    Create action creators for label

--- src/action-creators/index.ts
+++ src/action-creators/index.ts
@@ -1,3 +1,87 @@
 import actionTypes from '../constants/action-types';
+import {Label} from '../services';
 
-console.log(actionTypes);
+export function fetchLabel(dispatch) {
+  return new Promise((resolve) => {
+    const _labels = Label.cache();
+    const _action = {
+      type: actionTypes.FETCH_LABEL,
+      payload: {
+        labels: _labels,
+        isLoadingLabels: true,
+      },
+    };
+    dispatch(_action);
+
+    Label.fetch().then((labels: ILabel[]) => {
+      const action = {
+        type: actionTypes.FETCH_LABEL_SUCCESS,
+        payload: {
+          labels,
+          isLoadingLabels: false,
+        },
+      };
+      dispatch(action);
+      resolve(action);
+    });
+  });
+}
+
+export function createLabel(dispatch, label) {
+  return new Promise((resolve) => {
+    const _label = Label.build(label);
+    const _action = {
+      type: actionTypes.CREATE_LABEL,
+      payload: {label: _label},
+    };
+    dispatch(_action);
+
+    Label.create(_label).then((newLabel: ILabel) => {
+      const action = {
+        type: actionTypes.CREATE_LABEL_SUCCESS,
+        payload: {label: newLabel},
+      };
+      dispatch(action);
+      resolve(action);
+    });
+  });
+}
+
+export function updateLabel(dispatch, label) {
+  return new Promise((resolve) => {
+    const _label = Label.build(label);
+    const _action = {
+      type: actionTypes.UPDATE_LABEL,
+      payload: {label: _label},
+    };
+    dispatch(_action);
+
+    Label.update(_label).then((newLabel: ILabel) => {
+      const action = {
+        type: actionTypes.UPDATE_LABEL_SUCCESS,
+        payload: {label: newLabel},
+      };
+      dispatch(action);
+      resolve(action);
+    });
+  });
+}
+
+export function destroyLabel(dispatch, label) {
+  return new Promise((resolve) => {
+    const _action = {
+      type: actionTypes.DESTROY_LABEL,
+      payload: {label},
+    };
+    dispatch(_action);
+
+    Label.destroy(label).then(() => {
+      // const action = {
+      //   type: actionTypes.DESTROY_LABEL_SUCCESS,
+      //   payload: {label},
+      // };
+      // dispatch(action);
+      // resolve(action);
+    });
+  });
+}

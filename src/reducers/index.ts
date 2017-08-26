dad258e2d641360eb02b0c9cfbd8973d8428b4da
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-27 08:53:38 +0900

    Implement labels reducer

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,11 +1,67 @@
 import * as deepAssign from 'deep-assign';
 import actionTypes from '../constants/action-types';
 
-export default function (state: IState, action: IAction): IState {
+export default function(state: IState, action: IAction): IState {
   const newState: IState = deepAssign({}, state);
   const payload = action.payload;
   const meta = action.meta;
-  console.log(actionTypes, payload, meta);
+
+  switch (action.type) {
+    case (actionTypes.FETCH_LABEL): {
+      newState.labels = payload.labels;
+      newState.ui.isLoadingLabels = payload.isLoadingLabels;
+      break;
+    }
+    case (actionTypes.FETCH_LABEL_SUCCESS): {
+      newState.labels = payload.labels;
+      newState.ui.isLoadingLabels = payload.isLoadingLabels;
+      break;
+    }
+    case (actionTypes.CREATE_LABEL): {
+      newState.labels.push(payload.label);
+      newState.ui.isLoadingLabelCids.push(payload.label.cid);
+      break;
+    }
+    case (actionTypes.CREATE_LABEL_SUCCESS): {
+      newState.labels.push(payload.label);
+      newState.ui.isLoadingLabelCids = newState.ui.isLoadingLabelCids.filter((cid: string) => (cid !== payload.label.cid));
+      break;
+    }
+    case (actionTypes.UPDATE_LABEL): {
+      newState.labels = newState.labels.map((label: ILabel) => {
+        if (label.cid === payload.label.cid) {
+          return payload.label;
+        }
+        return label;
+      });
+      newState.ui.isLoadingLabelCids.push(payload.label.cid);
+      break;
+    }
+    case (actionTypes.UPDATE_LABEL_SUCCESS): {
+      newState.labels = newState.labels.map((label: ILabel) => {
+        if (label.cid === payload.label.cid) {
+          return payload.label;
+        }
+        return label;
+      });
+      newState.ui.isLoadingLabelCids = newState.ui.isLoadingLabelCids.filter((cid: string) => (cid !== payload.label.cid));
+      break;
+    }
+    case (actionTypes.DESTROY_LABEL): {
+      newState.labels = newState.labels.filter((label: ILabel) => {
+        if (label.cid === payload.label.cid) {
+          return false;
+        }
+        return true;
+      });
+      newState.ui.isLoadingLabelCids.push(payload.label.cid);
+      break;
+    }
+    case (actionTypes.DESTROY_LABEL_SUCCESS): {
+      newState.ui.isLoadingLabelCids = newState.ui.isLoadingLabelCids.filter((cid: string) => (cid !== payload.label.cid));
+      break;
+    }
+  }
 
   return newState;
 }

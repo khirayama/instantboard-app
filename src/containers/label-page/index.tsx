3c3617a14354be6ffed5a20017c2256c699c9577
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-27 22:58:34 +0900

    Implement label-page

--- src/containers/label-page/index.tsx
+++ src/containers/label-page/index.tsx
@@ -1,8 +1,63 @@
+import * as PropTypes from 'prop-types';
 import * as React from 'react';
+import {createLabel} from '../../action-creators';
 import Container from '../container';
 
 export default class LabelPage extends Container<any, any> {
+  public static contextTypes = {
+    move: PropTypes.func,
+  };
+
+  private handleChangeNameInput: any;
+
+  private handleSubmit: any;
+
+  constructor(props: any) {
+    super(props);
+
+    let name = '';
+    if (this.state.ui.selectedLabelCid) {
+      this.state.labels.forEach((label: ILabel) => {
+        if (label.cid === this.state.ui.selectedLabelCid) {
+          name = label.name;
+        }
+      });
+    }
+
+    this.state = Object.assign({}, this.state, {name});
+
+    this.actions = {
+      createLabel: (label) => {
+        createLabel(this.dispatch, label).then(() => {
+          this.context.move('/labels');
+        });
+      },
+    };
+
+    this.handleChangeNameInput = this._handleChangeNameInput.bind(this);
+    this.handleSubmit = this._handleSubmit.bind(this);
+  }
+
+  private _handleChangeNameInput(event: any) {
+    this.setState({name: event.currentTarget.value});
+  }
+
+  private _handleSubmit(event: any) {
+    event.preventDefault();
+    const name = this.state.name.trim();
+    if (name) {
+      this.actions.createLabel({name});
+    }
+  }
+
   public render() {
-    return <div>Label Page!</div>;
+    return (
+      <section className="page label-page">
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" autoFocus value={this.state.name} onChange={this.handleChangeNameInput} />
+          <button>Create!</button>
+        </form>
+      </section>
+    );
   }
 }

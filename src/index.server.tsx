199c25f8f3f80ab697e45e82e606cd4b693c1444
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-27 22:57:39 +0900

    Remove material icon from index.server.tsx

--- src/index.server.tsx
+++ src/index.server.tsx
@@ -21,7 +21,6 @@ function template(content) {
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
   <link rel="stylesheet" href="/index.css">
   <script src="/bundle.js" defer></script>
   <link rel="manifest" href="/manifest.json">

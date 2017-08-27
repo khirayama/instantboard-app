2bb29addfe29586a81043815677cd5733801d892
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-08-27 22:58:48 +0900

    Implement label-index-page

--- src/containers/label-index-page/index.tsx
+++ src/containers/label-index-page/index.tsx
@@ -33,11 +33,24 @@ export default class LabelIndexPage extends Container<any, any> {
     super(props);
 
     this.actions = {
+      fetchLabel: () => {
+        fetchLabel(this.dispatch);
+      },
+      updateLabel: (label: any) => {
+        updateLabel(this.dispatch, label);
+      },
+      destroyLabel: (label: any) => {
+        destroyLabel(this.dispatch, label);
+      },
       sortLabel: () => {},
     };
 
     this.handleClickCreateLabelButton = this._handleClickCreateLabelButton.bind(this);
     this.handleSortLabelList = this._handleSortLabelList.bind(this);
+  }
+
+  public componentDidMount() {
+    this.actions.fetchLabel();
   }
 
   public render() {
@@ -84,24 +97,13 @@ export default class LabelIndexPage extends Container<any, any> {
           className="label-list"
           onSort={this.handleSortLabelList}
         >
-          {labels.map((label: ILabel) => {
-            return (
-              <ListItem
+          {labels.map((label: ILabel) => (
+              <LabelListItem
                 key={label.cid}
-                className={classNames('label-list--item', {'label-list--item__unvisibled': !label.visibled})}
-              >
-                <div className="label-list--item--visible-button">
-                  <VisibilityIcon/>
-                </div>
-                <div className="label-list--item--content">
-                  <div className="label-list--item--content--text">{label.name}</div>
-                </div>
-                <div className="label-list--item--delete-button">
-                  <RemoveIcon/>
-                </div>
-              </ListItem>
-            );
-          })}
+                actions={this.actions}
+                label={label}
+              />
+          ))}
         </List>
       );
     }
@@ -122,5 +124,59 @@ export default class LabelIndexPage extends Container<any, any> {
 
   private _handleClickCreateLabelButton() {
     this.context.move('/labels/new');
+  }
+}
+
+class LabelListItem extends React.Component<any, any> {
+  private handleClickVisibleButton: any;
+
+  private handleClickDestroyButton: any;
+
+  constructor(props: any) {
+    super(props);
+
+    this.handleClickVisibleButton = this._handleClickVisibleButton.bind(this);
+    this.handleClickDestroyButton = this._handleClickDestroyButton.bind(this);
+  }
+
+  public render() {
+    const label = this.props.label;
+    const props = Object.assign({}, this.props);
+
+    delete props.actions;
+
+    return (
+      <ListItem
+        {...props}
+        className={classNames('label-list--item', {'label-list--item__unvisibled': !label.visibled})}
+      >
+        <div className="label-list--item--visible-button" onClick={this.handleClickVisibleButton}>
+          <Icon type="check" active={!label.visibled}/>
+        </div>
+        <div className="label-list--item--content">
+          <div className="label-list--item--content--text">{label.name}</div>
+        </div>
+        <div className="label-list--item--destroy-button" onClick={this.handleClickDestroyButton}>
+          <Icon type="remove" active={!label.visibled}/>
+        </div>
+      </ListItem>
+    );
+  }
+
+  private _handleClickVisibleButton() {
+    const label = this.props.label;
+    const actions = this.props.actions;
+
+    actions.updateLabel({
+      cid: label.cid,
+      visibled: !label.visibled,
+    });
+  }
+
+  private _handleClickDestroyButton() {
+    const label = this.props.label;
+    const actions = this.props.actions;
+
+    actions.destroyLabel(label);
   }
 }

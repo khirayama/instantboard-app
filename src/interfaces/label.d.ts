d106b3bb1e66dcf96ff0fdd34ffe546deee226a3
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-09-05 06:49:27 +0900

    Change schema and create new interface

--- src/interfaces/label.d.ts
+++ src/interfaces/label.d.ts
@@ -3,4 +3,29 @@ interface ILabel {
   name: string;
   visibled: boolean;
   priority: number;
+  members: IMember[];
+}
+
+interface ILabelRequest {
+  id?: string;
+  name?: string;
+  visibled?: boolean;
+  priority?: number;
+  members?: IMember[];
+}
+
+interface ILabelResponse {
+  id: number;
+  name: string;
+  visibled: boolean;
+  priority: number;
+  members: IMemberResponse[];
+  createdAt: string;
+  updatedAt: string;
+}
+
+interface IMember {
+}
+
+interface IMemberResponse {
 }
--- src/interfaces/task.d.ts
+++ src/interfaces/task.d.ts
@@ -1,11 +1,38 @@
+interface ISchedule {
+  shortMonthName: string;
+  shortDayName: string;
+  date: number;
+}
+
+interface IScheduleResponse {
+}
+
 interface ITask {
   id: string;
   labelId: string;
   text: string;
   completed: boolean;
-  schedule: {
-    shortMonthName: string;
-    shortDayName: string;
-    date: number;
-  } | null;
+  priority: number;
+  schedule: ISchedule | null;
+}
+
+interface ITaskRequest {
+  id?: string;
+  labelId?: string;
+  text?: string;
+  content?: string;
+  completed?: boolean;
+  priority?: number;
+  schedule?: ISchedule| null;
+}
+
+interface ITaskResponse {
+  id: string;
+  labelId: string;
+  text: string;
+  completed: boolean;
+  priority: number;
+  schedule: IScheduleResponse | null;
+  createdAt: string;
+  updatedAt: string;
 }

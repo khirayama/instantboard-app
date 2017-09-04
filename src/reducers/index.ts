c9ecef67b22ed051336a554979dd0b03cb8757b5
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-09-05 06:51:32 +0900

    Remove needless line

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -4,7 +4,6 @@ import actionTypes from '../constants/action-types';
 export default function (state: IState, action: IAction): IState {
   const newState: IState = deepAssign({}, state);
   const payload = action.payload;
-  // Const meta = action.meta;
 
   switch (action.type) {
     // LABEL

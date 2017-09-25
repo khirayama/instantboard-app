da02245e3307636c1bad984e3fe94726fd78983c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-09-26 07:03:59 +0900

    Update spinner-icon style

--- src/components/icon/spinner-icon.tsx
+++ src/components/icon/spinner-icon.tsx
@@ -5,6 +5,12 @@ export default class SpinnerIcon extends React.Component<any, any> {
     return (
       <div className="spinner-icon">
         <svg viewBox="0 0 32 32" width="32" height="32">
+          <defs>
+            <linearGradient id="spinner-gradient">
+              <stop className="spinner-color1" offset="100%"/>
+              <stop className="spinner-color2" offset="100%"/>
+            </linearGradient>
+          </defs>
           <circle id="spinner" cx="16" cy="16" r="14" fill="none"/>
         </svg>
       </div>

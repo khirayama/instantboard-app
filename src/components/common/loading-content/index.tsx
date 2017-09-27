9ddde230078bbbeee4c04f04b55f1c1ac51d419a
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-09-28 07:56:42 +0900

    Using new spinner in loading content

--- src/components/common/loading-content/index.tsx
+++ src/components/common/loading-content/index.tsx
@@ -1,12 +1,12 @@
 import * as React from 'react';
-import {SpinnerIcon} from '../../icon';
+import Spinner from '../../spinner';
 
 export default class LoadingContent extends React.Component<any, any> {
   public render() {
     return (
       <div className="loading-content">
         <div className="loading-content--spinner">
-          <SpinnerIcon/>
+          <Spinner/>
         </div>
       </div>
     );

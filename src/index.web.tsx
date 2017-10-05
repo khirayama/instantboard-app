e927d78bb4872b1498c4d36a19c5aab18ce90d99
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-10-06 06:52:39 +0900

    Add design metrics and keylines

--- src/index.web.tsx
+++ src/index.web.tsx
@@ -6,6 +6,7 @@ import Router from './router/router';
 import routes from './router/routes';
 import initialState from './store/initial-state';
 import Store from './store/store';
+import queryString from './utils/query-string';
 
 const store: IStore = new Store(initialState, reducers);
 const router = new Router(routes);
@@ -32,5 +33,31 @@ window.addEventListener('DOMContentLoaded', () => {
   const applicationLoadingElement: any = window.document.querySelector('.application--loader');
   if (applicationLoadingElement !== null) {
     applicationLoadingElement.parentNode.removeChild(applicationLoadingElement);
+  }
+
+  if (process && process.env.NODE_ENV !== 'production') {
+    const query = queryString.parse(location.search);
+    console.log(query);
+    if (query.metrics) {
+      applicationMainElement.insertAdjacentHTML(
+        'beforeend',
+        `
+        <div class="layout-metrics">
+          <div class="layout-metrics--grid layout-metrics--bar-low__top"></div>
+          <div class="layout-metrics--grid layout-metrics--bar-high__top"></div>
+          <div class="layout-metrics--grid layout-metrics--bar-low__bottom"></div>
+          <div class="layout-metrics--grid layout-metrics--bar-high__bottom"></div>
+          <div class="layout-metrics--grid layout-metrics--screen-edge__left"></div>
+          <div class="layout-metrics--grid layout-metrics--screen-edge__right"></div>
+          <div class="layout-metrics--grid layout-metrics--screen-edge--content__left"></div>
+          <div class="layout-metrics--grid layout-metrics--screen-edge--content__left--edge__left"></div>
+          <div class="layout-metrics--grid layout-metrics--screen-edge--content__left--edge__right"></div>
+          <div class="layout-metrics--grid layout-metrics--screen-edge--content__right"></div>
+          <div class="layout-metrics--grid layout-metrics--screen-edge--content__right--edge__left"></div>
+          <div class="layout-metrics--grid layout-metrics--screen-edge--content__right--edge__right"></div>
+        </div>
+        `
+      );
+    }
   }
 });

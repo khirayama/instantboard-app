b95a9f4d2241ed70a60114b868cd397f17cf7ba9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-10-12 22:32:52 +0900

    Create tracker class

---
+++ src/utils/tracker.ts
@@ -0,0 +1,20 @@
+declare var window: any;
+
+export default class Tracker {
+  private router: any;
+
+  constructor(router) {
+    this.router = router;
+  }
+
+  public send() {
+    if (typeof window === 'object' && window.ga) {
+      const {route} = this.router.matchRoute(window.location.pathname);
+      if (process && process.env.NODE_ENV === 'production') {
+        window.ga('send', 'pageview', route.path);
+      } else {
+        console.log(`%cSend as ${route.path}`, 'color: #9e9e9e;');
+      }
+    }
+  }
+}

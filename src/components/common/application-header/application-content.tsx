50e0ccedc34e5ed8f9b95e0511ebcda68aaae8b9
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-10-21 12:30:05 +0900

    Adjust application-header height

--- src/components/common/application-header/application-content.tsx
+++ src/components/common/application-header/application-content.tsx
@@ -3,7 +3,9 @@ import * as React from 'react';
 export default class ApplicationContent extends React.Component<any, any> {
   public render() {
     return (
-      <div className="application-content">{this.props.children}</div>
+      <div className="application-content">
+        <div className="application-content--inner">{this.props.children}</div>
+      </div>
     );
   }
 }

6ba8720d70fb7daf0df7dadd8d9455dbf5c478fa
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-11-14 08:36:32 +0900

    Replace div to Link in application-header

--- src/presentations/components/application-header/application-header.tsx
+++ src/presentations/components/application-header/application-header.tsx
@@ -1,30 +1,9 @@
 import * as classNames from 'classnames';
-import * as PropTypes from 'prop-types';
 import * as React from 'react';
 import Icon from '../icon';
+import Link from '../../../router/link';
 
 export default class ApplicationHeader extends React.Component<any, any> {
-  public static contextTypes = {
-    move: PropTypes.func,
-  };
-
-  private handleClickTaskTabLink: any;
-
-  private handleClickLabelTabLink: any;
-
-  private handleClickNotificationTabLink: any;
-
-  private handleClickProfileTabLink: any;
-
-  constructor(props: any) {
-    super(props);
-
-    this.handleClickTaskTabLink = this._handleClickTaskTabLink.bind(this);
-    this.handleClickLabelTabLink = this._handleClickLabelTabLink.bind(this);
-    this.handleClickNotificationTabLink = this._handleClickNotificationTabLink.bind(this);
-    this.handleClickProfileTabLink = this._handleClickProfileTabLink.bind(this);
-  }
-
   public render() {
     const currentIndex = this.props.index || 0;
     const badges = this.props.badges || [];
@@ -32,64 +11,48 @@ export default class ApplicationHeader extends React.Component<any, any> {
     return (
       <div className="application-header">
         <div className="application-header--content">
-          <div
+          <Link
+            to="/"
             className={classNames(
               'application-header-item',
               {'application-header-item__current': currentIndex === 0},
             )}
-            onClick={this.handleClickTaskTabLink}
           >
             <Icon type="list" className="application-header--icon"/>
             {(badges.indexOf(0) !== -1) ? (<span className="application-header--icon--badge"/>) : null}
-          </div>
-          <div
+          </Link>
+          <Link
+            to="/labels"
             className={classNames(
               'application-header-item',
               {'application-header-item__current': currentIndex === 1},
             )}
-            onClick={this.handleClickLabelTabLink}
           >
             <Icon type="label" className="application-header--icon"/>
             {(badges.indexOf(1) !== -1) ? (<span className="application-header--icon--badge"/>) : null}
-          </div>
-          <div
+          </Link>
+          <Link
+            to="/notifications"
             className={classNames(
               'application-header-item',
               {'application-header-item__current': currentIndex === 2},
             )}
-            onClick={this.handleClickNotificationTabLink}
           >
             <Icon type="notification" className="application-header--icon"/>
             {(badges.indexOf(2) !== -1) ? (<span className="application-header--icon--badge"/>) : null}
-          </div>
-          <div
+          </Link>
+          <Link
+            to="/profile"
             className={classNames(
               'application-header-item',
               {'application-header-item__current': currentIndex === 3},
             )}
-            onClick={this.handleClickProfileTabLink}
           >
             <Icon type="profile" className="application-header--icon"/>
             {(badges.indexOf(3) !== -1) ? (<span className="application-header--icon--badge"/>) : null}
-          </div>
+          </Link>
         </div>
       </div>
     );
-  }
-
-  private _handleClickTaskTabLink() {
-    this.context.move('/');
-  }
-
-  private _handleClickLabelTabLink() {
-    this.context.move('/labels');
-  }
-
-  private _handleClickNotificationTabLink() {
-    this.context.move('/notifications');
-  }
-
-  private _handleClickProfileTabLink() {
-    this.context.move('/profile');
   }
 }

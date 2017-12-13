4d8e7364a64b6e8906938affa2fa05c622716760
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2017-12-14 06:33:33 +0900

    Refactor search-member-list-item

--- src/presentations/components/search-member-list-item/index.tsx
+++ src/presentations/components/search-member-list-item/index.tsx
@@ -2,28 +2,20 @@ import * as React from 'react';
 import Icon from '../icon';
 
 export default class SearchMemberListItem extends React.Component<any, any> {
-  private handleClick: any;
-
-  constructor(props: any) {
-    super(props);
-
-    this.handleClick = this._handleClick.bind(this);
-  }
-
   public render() {
     const member = this.props.member;
+
+    const handleClick = (event: any) => {
+      if (this.props.onClick) {
+        this.props.onClick(event, this.props, this.state);
+      }
+    };
+
     return (
-      <li
-        onClick={this.handleClick}
-      >
+      <li onClick={handleClick}>
         <Icon type="profile"/>
         <p>{member.name}</p>
       </li>
     );
-  }
-
-  private _handleClick() {
-    const member = this.props.member;
-    this.props.actions.setMemberName(member.name);
   }
 }

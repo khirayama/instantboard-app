1167e4682389ed799592af26b4ee13c93869bb17
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-02-08 08:48:37 +0900

    Remove needless lines

--- src/index.web.tsx
+++ src/index.web.tsx
@@ -6,7 +6,6 @@ import Router from './router/router';
 import routes from './router/routes';
 import initialState from './store/initial-state';
 import Store from './store/store';
-import queryString from './utils/query-string';
 import tokenManager from './utils/token-manager';
 import Tracker from './utils/tracker';
 
@@ -41,38 +40,5 @@ window.addEventListener('DOMContentLoaded', () => {
   const applicationLoadingElement: HTMLElement | null = window.document.querySelector('.application--loader');
   if (applicationLoadingElement !== null && applicationLoadingElement.parentNode) {
     applicationLoadingElement.parentNode.removeChild(applicationLoadingElement);
-  }
-
-  if (process && process.env.NODE_ENV !== 'production') {
-    const query: any = queryString.parse(location.search);
-
-    if (query.metrics && applicationMainElement !== null) {
-      applicationMainElement.insertAdjacentHTML(
-        'beforeend',
-        `
-        <div class="layout-metrics">
-          <div class="layout-metrics--grid layout-metrics--grid--bar layout-metrics--grid__high__top"></div>
-          <div class="layout-metrics--grid layout-metrics--grid--bar layout-metrics--grid__low__top"></div>
-
-          <div class="layout-metrics--grid layout-metrics--grid--bar layout-metrics--grid__high__bottom"></div>
-          <div class="layout-metrics--grid layout-metrics--grid--bar layout-metrics--grid__low__bottom"></div>
-
-          <div class="layout-metrics--grid layout-metrics--grid--line layout-metrics--grid__screen-edge__left"></div>
-          <div class="layout-metrics--grid layout-metrics--grid--line layout-metrics--grid__screen-edge__right"></div>
-
-          <div class="layout-metrics--grid layout-metrics--grid--line layout-metrics--grid__screen-edge-content__left">
-          </div>
-          <div class="layout-metrics--grid layout-metrics--grid--line layout-metrics--grid__screen-edge-content__right">
-          </div>
-
-          <div class="layout-metrics--grid layout-metrics--grid--content__left"></div>
-          <div class="layout-metrics--grid layout-metrics--grid--content__right"></div>
-
-          <div class="layout-metrics--grid layout-metrics--grid--content--edge__left"></div>
-          <div class="layout-metrics--grid layout-metrics--grid--content--edge__right"></div>
-        </div>
-        `,
-      );
-    }
   }
 });

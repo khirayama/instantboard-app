f500df63024ede2ccf9b77d39f3bf2a6a9bd6534
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-02-08 09:19:34 +0900

    Create bin/minimize-images.js script

---
+++ bin/minimize-images.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const imagemin = require('imagemin');
+const imageminJpegtran = require('imagemin-jpegtran');
+const imageminPngquant = require('imagemin-pngquant');
+const imageminWebp = require('imagemin-webp');
+
+const ROOT_DIR = './materials';
+const DIST_DIR = './src/assets';
+
+function isTarget(filePath) {
+  return (
+    filePath.endsWith('.png') || filePath.endsWith('.jpg') || filePath.endsWith('.jpeg') || filePath.endsWith('.webp')
+  );
+}
+
+function compressImage(filePath) {
+  console.log(`Compressing ${filePath}`);
+  const tmpDist = filePath.split('/');
+  tmpDist.pop();
+  const dist = tmpDist.join('/').replace(ROOT_DIR, DIST_DIR);
+  imagemin([filePath], dist, {
+    plugins: [
+      imageminJpegtran(),
+      imageminPngquant({
+        quality: '60-70',
+      }),
+    ],
+  });
+  imagemin([filePath], dist, {
+    plugins: [
+      imageminWebp({
+        quality: '60-70',
+        lossless: true,
+      }),
+    ],
+  });
+}
+
+function compressImages(rootPath) {
+  fs.readdir(rootPath, (err, fileNames) => {
+    for (let i = 0; i < fileNames.length; i++) {
+      const fileName = fileNames[i];
+      const filePath = rootPath + '/' + fileName;
+      const stats = fs.statSync(filePath);
+      if (stats.isDirectory()) {
+        compressImages(filePath);
+      } else if (isTarget(filePath)) {
+        compressImage(filePath);
+      }
+    }
+  });
+}
+
+compressImages(ROOT_DIR);

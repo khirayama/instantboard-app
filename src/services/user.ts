7d36b97088adc12d00fa0bd7856515f799cf6081
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-02-26 07:57:47 +0900

    Create user file as service

---
+++ src/services/user.ts
@@ -0,0 +1,63 @@
+import { AxiosInstance, AxiosError } from 'axios';
+import createRequest from './utils/create-request';
+import handleRequestError from './utils/handle-request-error';
+
+const searchReq = () => createRequest('/api/v1/search');
+
+const User = {
+  req: (): AxiosInstance => createRequest('/api/v1/user'),
+
+  get: (): Promise<IUserResponse> => {
+    return new Promise((resolve, reject): void => {
+      User.req()
+        .get('/')
+        .then(({ data }): void => {
+          resolve(data);
+        })
+        .catch((err: AxiosError): void => {
+          handleRequestError(err, reject);
+        });
+    });
+  },
+
+  search: (params: { q: string }): Promise<IMemberResponse[]> => {
+    return new Promise((resolve, reject): void => {
+      searchReq()
+        .get('/users', { params })
+        .then(({ data }): void => {
+          resolve(data);
+        })
+        .catch((err: AxiosError): void => {
+          handleRequestError(err, reject);
+        });
+    });
+  },
+
+  delete: (): Promise<void> => {
+    return new Promise((resolve, reject): void => {
+      User.req()
+        .delete('/')
+        .then((): void => {
+          resolve();
+        })
+        .catch((err: AxiosError): void => {
+          handleRequestError(err, reject);
+        });
+    });
+  },
+
+  fetchMember: (): Promise<IMemberResponse[]> => {
+    return new Promise((resolve, reject): void => {
+      User.req()
+        .get('/members')
+        .then(({ data }): void => {
+          resolve(data);
+        })
+        .catch((err: AxiosError): void => {
+          handleRequestError(err, reject);
+        });
+    });
+  },
+};
+
+export default User;

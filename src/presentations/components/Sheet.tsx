63744192987f6f19927592c0f9ab82dff242de2b
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-05-03 07:09:37 +0900

    Refactor Sheet component

--- src/presentations/components/Sheet.tsx
+++ src/presentations/components/Sheet.tsx
@@ -1,25 +1,13 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
 
-interface ISheetState {
-  isShown: boolean;
-}
-
 interface ISheetProps {
   isShown: boolean;
 }
 
-export class Sheet extends React.Component<ISheetProps, ISheetState> {
-  constructor(props: ISheetProps) {
-    super(props);
-
-    this.state = {
-      isShown: this.props.isShown,
-    };
-  }
-
+export class Sheet extends React.Component<ISheetProps, {}> {
   public render(): JSX.Element | null {
-    const isShown: boolean = this.state.isShown;
+    const isShown: boolean = this.props.isShown;
 
     return isShown ? (
       <div className="sheet">

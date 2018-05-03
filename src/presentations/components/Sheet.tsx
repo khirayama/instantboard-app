8f39628de5e0eb0e485265154a73f9d3885540af
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-05-03 15:25:58 +0900

    Add animation effects for Sheet component

--- src/presentations/components/Sheet.tsx
+++ src/presentations/components/Sheet.tsx
@@ -1,18 +1,49 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
+import { CSSTransition } from 'react-transition-group';
+
+import { TRANSITION_TIME_BASIC } from 'presentations/constants';
 
 interface ISheetProps {
   isShown: boolean;
+  onClickBackground(event: React.MouseEvent<HTMLElement>): void;
 }
 
 export class Sheet extends React.Component<ISheetProps, {}> {
-  public render(): JSX.Element | null {
-    const isShown: boolean = this.props.isShown;
+  private onClickContent: (event: React.MouseEvent<HTMLElement>) => void;
 
-    return isShown ? (
-      <div className="sheet">
-        <div className="sheet--content">{this.props.children}</div>
-      </div>
-    ) : null;
+  constructor(props: ISheetProps) {
+    super(props);
+
+    this.onClickContent = this.handleClickContent.bind(this);
+  }
+
+  public render(): JSX.Element {
+    const { isShown, onClickBackground } = this.props;
+
+    return (
+      <CSSTransition
+        in={isShown}
+        timeout={TRANSITION_TIME_BASIC}
+        classNames={{
+          enter: 'sheet__enter',
+          enterActive: 'sheet__enter-active',
+          enterDone: 'sheet__enter-done',
+          exit: 'sheet__exit',
+          exitActive: 'sheet__exit-active',
+          exitDone: 'sheet__exit-done',
+        }}
+      >
+        <div role="button" className="sheet" onClick={onClickBackground}>
+          <div role="button" className="sheet--content" onClick={this.onClickContent}>
+            {this.props.children}
+          </div>
+        </div>
+      </CSSTransition>
+    );
+  }
+
+  private handleClickContent(event: React.MouseEvent<HTMLElement>): void {
+    event.stopPropagation();
   }
 }

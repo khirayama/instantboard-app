04ce794bc1a8085898152763834a0bc2b454480d
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-05-03 16:15:55 +0900

    Refactor Sheet component

--- src/presentations/components/Sheet.tsx
+++ src/presentations/components/Sheet.tsx
@@ -1,6 +1,6 @@
 import * as PropTypes from 'prop-types';
 import * as React from 'react';
-import { CSSTransition } from 'react-transition-group';
+import { CSSTransition, TransitionGroup } from 'react-transition-group';
 
 import { TRANSITION_TIME_BASIC } from 'presentations/constants';
 
@@ -18,28 +18,35 @@ export class Sheet extends React.Component<ISheetProps, {}> {
     this.onClickContent = this.handleClickContent.bind(this);
   }
 
-  public render(): JSX.Element {
+  public render(): JSX.Element | null {
     const { isShown, onClickBackground } = this.props;
 
     return (
-      <CSSTransition
-        in={isShown}
-        timeout={TRANSITION_TIME_BASIC}
-        classNames={{
-          enter: 'sheet__enter',
-          enterActive: 'sheet__enter-active',
-          enterDone: 'sheet__enter-done',
-          exit: 'sheet__exit',
-          exitActive: 'sheet__exit-active',
-          exitDone: 'sheet__exit-done',
-        }}
-      >
-        <div role="button" className="sheet" onClick={onClickBackground}>
-          <div role="button" className="sheet--content" onClick={this.onClickContent}>
-            {this.props.children}
-          </div>
-        </div>
-      </CSSTransition>
+      <TransitionGroup>
+        {isShown ? (
+          <CSSTransition
+            in={isShown}
+            timeout={TRANSITION_TIME_BASIC}
+            classNames={{
+              appear: 'sheet__enter',
+              appearActive: 'sheet__enter-active',
+              appearDone: 'sheet__enter-done',
+              enter: 'sheet__enter',
+              enterActive: 'sheet__enter-active',
+              enterDone: 'sheet__enter-done',
+              exit: 'sheet__exit',
+              exitActive: 'sheet__exit-active',
+              exitDone: 'sheet__exit-done',
+            }}
+          >
+            <div role="button" className="sheet" onClick={onClickBackground}>
+              <div role="button" className="sheet--content" onClick={this.onClickContent}>
+                {this.props.children}
+              </div>
+            </div>
+          </CSSTransition>
+        ) : null}
+      </TransitionGroup>
     );
   }
 

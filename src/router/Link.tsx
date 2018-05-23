d29c831761f265f50dca04c27f7a546edb5f9fed
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-05-24 08:39:44 +0900

    Refactor Link component

--- src/router/Link.tsx
+++ src/router/Link.tsx
@@ -1,4 +1,3 @@
-import * as PropTypes from 'prop-types';
 import * as React from 'react';
 
 import { context } from 'router/Navigator';
@@ -9,29 +8,40 @@ interface ILinkProps {
 }
 
 export class Link extends React.Component<ILinkProps> {
+  public move: any;
+
+  private onClick: (event: React.MouseEvent<HTMLElement>) => void;
+
+  constructor(props: any) {
+    super(props);
+
+    this.onClick = this.handleClick.bind(this);
+  }
+
   public render(): JSX.Element {
     const { children, className, to } = this.props;
 
-    /* tslint:disable:react-this-binding-issue */
     return (
-      <context.Consumer>
-        {({ move }: any): JSX.Element => {
-          return (
-            <a
-              href={to}
-              className={className}
-              onClick={(event: any): void => {
-                event.preventDefault();
-                event.stopPropagation();
-                move(to);
-              }}
-            >
-              {children}
-            </a>
-          );
-        }}
-      </context.Consumer>
+      <>
+        <context.Consumer>{this.bindContext.bind(this)}</context.Consumer>
+        <a href={to} className={className} onClick={this.onClick}>
+          {children}
+        </a>
+      </>
     );
-    /* tslint:enable:react-this-binding-issue */
+  }
+
+  private bindContext(ctx: any): null {
+    this.move = ctx.move;
+
+    return null;
+  }
+
+  private handleClick(event: React.MouseEvent<HTMLElement>): void {
+    const { to } = this.props;
+
+    event.preventDefault();
+    event.stopPropagation();
+    this.move(to);
   }
 }

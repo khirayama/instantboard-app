e652c4325c19840d4d19263c75067c038dc0f96c
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2018-05-26 08:23:40 +0900

    Update service-worker.js

--- src/assets/service-worker.js
+++ src/assets/service-worker.js
@@ -1,12 +1,12 @@
 self.addEventListener('install', event => {
-  console.log('installing');
+  console.log('%cInstalling', 'color: #9e9e9e;');
   self.skipWaiting();
 });
 
 self.addEventListener('activate', event => {
-  console.log('activating');
+  console.log('%cActivating', 'color: #9e9e9e;');
 });
 
 self.addEventListener('fetch', event => {
-  console.log('fetching', event.request.url);
+  console.log(`%cFetching ${event.request.url}`, 'color: #9e9e9e;');
 });
